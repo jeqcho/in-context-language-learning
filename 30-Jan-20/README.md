6e8c83324d9d7a0a26f6876286db97c1e0fa029a
jeqcho <jchooi@holygpu8a19205.rc.fas.harvard.edu>
2025-01-20 20:50:59 -0500

    training pomegranate hmm

---
+++ 30-Jan-20/README.md
@@ -0,0 +1,30 @@
+# Jan 20
+
+This continues the work from `29-Dec-4/README.md`.
+
+To recap, we have the TinyStories datsets cleaned and tokenized on a word level. We included datasets for sentences using the top 100, 200, 300, 400, 500 words. Here is the one for top 100.
+- `/n/netscratch/sham_lab/Everyone/jchooi/in-context-language-learning/data/TinyStories-100-train.txt`
+- `/n/netscratch/sham_lab/Everyone/jchooi/in-context-language-learning/data/TinyStories-100-test.txt`
+
+The file looks like this
+
+```
+18 11 4 3 27 54 22 19 40 5 1 62 0
+7 36 24 31 0
+18 11 4 3 54 22 19 40 5 1 62 5 29 0
+18 11 4 3 27 54 22 19 40 5 1 62 13 14 46 0
+8 17 3 55 22 87 0
+7 33 3 16 50 12 3 61 0
+95 4 7 40 82 5 45 68 96 0
+```
+
+The final step is to train the HMMs. This folder will train the HMMs using GPU via pomegranate.
+
+We will fit the HMMs with the following parameters:
+- Emissions (E): 100, 200, 300, 400
+- Hidden states (H): 100, 200, 300, 400
+- Sequence lengths (L): 100, 200, 300, 400
+
+This work is done at `train_hmm.py`.
+
+Testing on 1000 to 4000 rows, it seems like the model can train under 3 minutes. I will test this out.
---
+++ 30-Jan-20/train_hmm.py
@@ -0,0 +1,78 @@
+# %%
+from typing import Any, List
+from pomegranate.hmm import DenseHMM
+from pomegranate.distributions import Categorical
+import numpy as np
+from dataclasses import dataclass
+import torch as t
+import json
+
+# %%
+# initialize the model
+
+sequence = "CGACTACTGACTACTCGCCGACGCGACTGCCGTCTATACTGCGCATACGGC"
+X = np.array([[[["A", "C", "G", "T"].index(char)] for char in sequence]])
+
+
+@dataclass
+class HMMArgs:
+    num_emissions: int
+    num_states: int
+    seq_length: int
+
+
+def init_model(hmm_args: HMMArgs) -> DenseHMM:
+    hidden_states: List[Any] = [None] * hmm_args.num_states
+    for i in range(hmm_args.num_states):
+        hidden_states[i] = Categorical(
+            t.full(size=(1, hmm_args.num_emissions), fill_value=1.0 / hmm_args.num_emissions)
+        )
+    edges = t.full(size=(hmm_args.num_states, hmm_args.num_states), fill_value=1.0 / hmm_args.num_states)
+    starts = t.full(size=(hmm_args.num_states,), fill_value=1.0 / hmm_args.num_states)
+    ends = t.full(size=(hmm_args.num_states,), fill_value=1.0 / hmm_args.num_states)
+
+    model = DenseHMM(hidden_states, edges=edges, starts=starts, ends=ends, verbose=True)
+    return model
+
+
+# %%
+
+if __name__ == "__main__":
+    # init params
+    hmm_args = HMMArgs(num_emissions=100, num_states=100, seq_length=100)
+
+    # get training data
+    train_fname = f"/n/netscratch/sham_lab/Everyone/jchooi/in-context-language-learning/data/TinyStories-{hmm_args.num_emissions}-train.txt"
+    test_fname = f"/n/netscratch/sham_lab/Everyone/jchooi/in-context-language-learning/data/TinyStories-{hmm_args.num_emissions}-test.txt"
+
+    with open(train_fname, "r") as f:
+        train_lines = f.readlines()
+
+    # concat into a big string and split into seq_length
+    train_string = "".join(train_lines)
+    train_string = train_string.replace("\n", "")
+    train_integers = [int(token) for token in train_string.split(" ")]
+
+    # remove trailing sequence
+    extra_length = len(train_integers) % hmm_args.seq_length
+    train_integers = train_integers[:-extra_length]
+    train_array: np.ndarray = np.array(train_integers).reshape(-1, hmm_args.seq_length)
+
+    # wrap each emission as 1d
+    train_array = np.expand_dims(train_array, -1)
+    
+    # train_array.shape is (65930, 100, 1)
+
+    # test this out by getting a small set
+    # train_array = train_array[:4000]
+
+    # init model
+    model = init_model(hmm_args)
+
+    # train model
+    model.fit(train_array)
+
+    # save model
+    model_fname = f"/n/netscratch/sham_lab/Everyone/jchooi/in-context-language-learning/models/hmm-H-{hmm_args.num_states}-E-{hmm_args.num_emissions}-L-{hmm_args.seq_length}.json"
+
+    t.save(model, model_fname)
